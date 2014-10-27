b396dffc2f15d9d44001971a8af8c48dff6ebd13
ChanderG <chandergovind@gmail.com>
2014-10-27 20:59:05 +0530

    Added test files

---
+++ tests/maintest.c
@@ -0,0 +1,88 @@
+//Hi, simple test file.
+/*
+int print(int a,double c){
+  //char b;  
+  //double g,h;
+  //b = a / c;
+  //a = 5.37;
+  //a = 'a';
+  //b = a++;
+  return 1;
+}
+*/
+void main(){
+  //int a = 3,*b,c = 5; //,*b;
+  //char *c,d;
+  //int e,f;
+  //double g,h = 6.7;
+
+  //x = 2;
+  //y = 5;
+
+  //a = x*y;
+  //a = x / y;
+  //a = x + y;
+  //a = x - y;
+  //++a;
+
+  //int x;
+  //a = +x;
+  //a = -x;
+  //a = *x;
+  //a = &x;
+  //not yet supported
+  //*a = x;
+
+  //a < x;
+  //a > x;
+  //a >= x;
+  //a <= x;
+  //a == x;
+  //a != x;
+
+  //(a<=x) && (a>=x) ;
+  //a < x && a > x;
+  //a < x || a > x;
+ 
+  //if(a < 3) a = b;
+ 
+  /*
+  if(a < 3){
+    a = b; 
+  }else{
+    c = b;
+  }
+  */
+
+  /*
+  while(a < b){
+    b = a;
+  }
+  */
+
+  /*
+  do{
+    b = a;
+    //if(a < 3) a = b;
+  }while(a < b);  
+  */
+
+  /* 
+  int i;
+  for(i = 0;i < 3;i = i + 1){
+    a = i;
+  }
+  */
+
+  //hello(a,b);
+
+  //c = hello(a);
+  //a = b[10][10];
+  
+  //int a[10][5],b = 3;
+  //char c;
+
+  //a = (i<3);
+
+  return; 
+}
